4ea856c43f33424340ca49de6c8b1310fc766275
steelbridge-io <chris@steelbridge.io>
2026-01-14 00:16:48 -0800

    Update header logos to include both red and white versions for scroll and no-scroll states

--- header.php
+++ header.php
@@ -67,7 +67,9 @@ if ( is_front_page() ) {
 
                 <!-- Brand/Logo (Centered) -->
                 <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                 <!-- Red Logo -->
                     <img class="tfs-nav-logo scroll" loading="eager" src="https://tfs-spaces.sfo2.digitaloceanspaces.com/theflyshop/uploads/2021/05/social_tfs_logo_og.png" alt="The Fly Shop 2025" />
+                 <!-- White Logo -->
                     <img class="tfs-nav-logo no-scroll" loading="eager" src="https://tfs-spaces.sfo2.digitaloceanspaces.com/theflyshop/uploads/2017/06/TFSLogo.png" alt="The Fly Shop 2025" />
                 </a>
 
@@ -115,10 +117,12 @@ if ( is_front_page() ) {
 <?php if (is_page_template('page-templates/staff-template.php')) : ?>
         <div id="below-nav-logo" class="below-nav-logo-container">
             <a href="<?php echo esc_url(home_url('/')); ?>">
+             <!-- Red Logo -->
                 <img class="tfs-nav-logo scroll" loading="eager" src="https://tfs-spaces.sfo2.digitaloceanspaces.com/theflyshop/uploads/2021/05/social_tfs_logo_og.png" alt="The Fly Shop 2025" />
 <?php if ($tfs_header_logo !== '') : ?>
                     <img class="tfs-nav-logo no-scroll mb-5" loading="eager" src="<?php echo $tfs_header_logo; ?>" alt="The Fly Shop 2025" />
 <?php else: ?>
+             <!-- White Logo -->
                 <img class="tfs-nav-logo no-scroll" loading="eager" src="https://tfs-spaces.sfo2.digitaloceanspaces.com/theflyshop/uploads/2025/09/tfs-logo-600x484-1-1.png" alt="The Fly Shop 2025" />
 <?php endif; ?>
             </a>
