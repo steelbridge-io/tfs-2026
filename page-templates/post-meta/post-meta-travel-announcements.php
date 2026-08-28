46ad6d6be94bf23e83561558a08ec9a85ce72f28
steelbridge-io <chris@steelbridge.io>
2026-08-28 06:45:49 -0700

    Add background position meta to travel-announcements template with default fallback

--- page-templates/post-meta/post-meta-travel-announcements.php
+++ page-templates/post-meta/post-meta-travel-announcements.php
@@ -5,4 +5,8 @@
  * @package The_Fly_Shop
  */
 
-$travel_announcements_bg_image = get_post_meta(get_the_ID(), 'travel-announcements-bg-image', true);
+$travel_announcements_bg_image    = get_post_meta(get_the_ID(), 'travel-announcements-bg-image', true);
+$travel_announcements_bg_position = get_post_meta(get_the_ID(), 'travel-announcements-bg-position', true);
+if ($travel_announcements_bg_position === '' || $travel_announcements_bg_position === false) {
+	$travel_announcements_bg_position = 50;
+}
